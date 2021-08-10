9e0d74453c5fffd407b8045d2467b9251afceeb0
Lukáš Růžička <lruzicka@redhat.com>
2021-08-10 13:50:19 +0200

    Create a test suite for Loupe (Image Viewer)
    This PR adds a test for a new Image Viewer called Loupe.
    It is based on the old Image Viewer test, newly reneedled
    with some of the tests shortened, deleted or edited
    as the new Image Viewer has a little bit less functions
    compared to the previous one.
    
    Add needles.

---
+++ tests/applications/loupe/browse.pm
@@ -0,0 +1,25 @@
+use base "installedtest";
+use strict;
+use testapi;
+use utils;
+
+# This part tests if the application can navigate through the current folder.
+
+sub run {
+    my $self = shift;
+
+    # Go to next picture.
+    send_key("right");
+    assert_screen("loupe_image_next", timeout => 90);
+    # Go to previous picture
+    send_key("left");
+    assert_and_click("loupe_image_default", timeout => 90);
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+1;
+
+# vim: set sw=4 et:
