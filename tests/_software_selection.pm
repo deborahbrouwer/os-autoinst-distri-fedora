5b3e90e032e14b77c0f9c7b1bde4b6640bbda9c7
Adam Williamson <awilliam@redhat.com>
2022-12-13 12:03:41 -0800

    _software_selection: simplify a grep (F35 is EOL)
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -21,11 +21,9 @@ sub run {
         elsif (get_var('SUBVARIANT') eq 'Workstation') {
             $env = 'workstation-product-environment';
         }
-        # pre-F35 line looks like:
-        # 07:51:39,382 INF modules.payloads.payload.dnf.utils: selected environment: custom-environment
-        # F35+ line looks like:
+        # line looks like:
         # 07:40:26,614 DBG ui.lib.software: Selecting the 'custom-environment' environment.
-        assert_script_run "egrep '(selected env|Selecting the.*environment)' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
+        assert_script_run "grep 'Selecting the.*environment' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
         send_key "ctrl-alt-f6";
         assert_screen "anaconda_main_hub", 30;
         return;
