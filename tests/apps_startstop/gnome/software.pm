a64cadb8fe728336cb9d0fc3d3e2c0718390cb00
Adam Williamson <awilliam@redhat.com>
2023-01-11 09:16:31 -0800

    Make the Software welcome->ignore change in apps_startstop too
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/apps_startstop/gnome/software.pm
+++ tests/apps_startstop/gnome/software.pm
@@ -12,8 +12,12 @@ sub run {
     start_with_launcher('apps_menu_software');
 
 
-    # check if a welcome screen appears, if so, click on it
-    send_key 'ret' if (check_screen('gnome_software_welcome', 10));
+    # check if third party dialog appears, if so, click it away
+    if (check_screen 'gnome_software_ignore', 10) {
+        wait_still_screen 3;
+        # match again as the dialog may have moved a bit
+        assert_and_click 'gnome_software_ignore';
+    }
     assert_screen 'desktop_package_tool_update';
     # Register application
     register_application("gnome-software");
