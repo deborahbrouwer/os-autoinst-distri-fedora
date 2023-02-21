7ea4ed733c32b05bba69454b748ef9482ccfe457
Adam Williamson <awilliam@redhat.com>
2023-02-20 17:29:13 -0800

    Sigh, do the are-we-44 check in the right place
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -867,14 +867,14 @@ sub gnome_initial_setup {
     send_key "ret";
     if ($args{prelogin}) {
         # create user
-        # temporarily (until the versions are stable) we need to check
-        # whether we're on 43 or 44 here
-        my $extratab = 0;
-        $extratab = 1 if (check_screen 'initial_setup_44', 5);
         my $user_login = get_var("USER_LOGIN") || "test";
         my $user_password = get_var("USER_PASSWORD") || "weakpassword";
         type_very_safely $user_login;
         wait_screen_change { assert_and_click "next_button"; };
+        # temporarily (until the versions are stable) we need to check
+        # whether we're on 43 or 44 here
+        my $extratab = 0;
+        $extratab = 1 if (check_screen 'initial_setup_44', 5);
         type_very_safely $user_password;
         send_key "tab";
         # on GNOME 44+ we need a second tab here to reach the confirm box
