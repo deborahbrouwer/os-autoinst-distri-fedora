426747c36892c18a665da9368dde50453492be12
Adam Williamson <awilliam@redhat.com>
2023-06-18 11:15:56 +0200

    Adjust modularity_tests to enable the modular repos at the start
    As of yesterday's Rawhide, the modular repos are not installed by
    default, so of course all the modular tests fail. So, install
    the repos before running the tests.
    
    This isn't conditionalized on release version as I don't think
    we ever run this test on anything other than Rawhide and
    Branched.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

---
+++ tests/modularity_setup.pm
@@ -0,0 +1,20 @@
+use base "installedtest";
+use strict;
+use modularity;
+use testapi;
+use utils;
+
+sub run {
+    # switch to tty and login as root
+    $self->root_console(tty => 3);
+    # modular repos are not installed by default since F39
+    assert_script_run "dnf -y install fedora-repos-modular";
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
+
+# vim: set sw=4 et:
