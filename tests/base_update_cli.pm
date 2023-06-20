0a87a76ff90e0cb565c11b12712896e2e0095a44
Adam Williamson <awilliam@redhat.com>
2023-06-20 13:06:36 +0200

    Run base tests on ELN
    This requires a change in the package we use for base_update_cli
    because pandoc-common is not in ELN.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/base_update_cli.pm
+++ tests/base_update_cli.pm
@@ -11,16 +11,16 @@ sub run {
     prepare_test_packages;
     # check rpm agrees they installed good
     verify_installed_packages;
-    # update the fake pandoc-common (should come from the real repo)
+    # update the fake acpica-tools (should come from the real repo)
     # this can take a long time if we get unlucky with the metadata refresh
-    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing update pandoc-common', 600;
+    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing update acpica-tools', 600;
     # check we got the updated version
     verify_updated_packages;
-    # now remove pandoc-common, and see if we can do a straight
+    # now remove acpica-tools, and see if we can do a straight
     # install from the default repos
-    assert_script_run 'dnf -y remove pandoc-common';
-    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing install pandoc-common', 120;
-    assert_script_run 'rpm -V pandoc-common';
+    assert_script_run 'dnf -y remove acpica-tools';
+    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing install acpica-tools', 120;
+    assert_script_run 'rpm -V acpica-tools';
 }
 
 sub test_flags {
