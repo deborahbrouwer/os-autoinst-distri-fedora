0d8df76b1b2938f03870f9028a3804c2fb36a681
Adam Williamson <awilliam@redhat.com>
2023-08-23 10:34:03 -0700

    apps_startstop: new GNOME image viewer is not in Utilities
    It's on the top level.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/apps_startstop/gnome/imageviewer.pm
+++ tests/apps_startstop/gnome/imageviewer.pm
@@ -9,7 +9,7 @@ sub run {
     my $self = shift;
 
     # Start the application
-    start_with_launcher('apps_menu_imageviewer', 'apps_menu_utilities');
+    start_with_launcher('apps_menu_imageviewer');
     # Check that is started
     assert_screen 'apps_run_imageviewer';
     # Register application
