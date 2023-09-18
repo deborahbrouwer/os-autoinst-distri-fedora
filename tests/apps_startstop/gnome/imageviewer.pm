836f8e2da6f62fd7905ee702a3a1d210ac37d096
Adam Williamson <awilliam@redhat.com>
2023-09-18 08:47:18 -0700

    Revert "apps_startstop: new GNOME image viewer is not in Utilities"
    This reverts commit 0d8df76b1b2938f03870f9028a3804c2fb36a681.
    Loupe now seems to have been moved down into Utilities like eog
    was, so revert this.

--- tests/apps_startstop/gnome/imageviewer.pm
+++ tests/apps_startstop/gnome/imageviewer.pm
@@ -9,7 +9,7 @@ sub run {
     my $self = shift;
 
     # Start the application
-    start_with_launcher('apps_menu_imageviewer');
+    start_with_launcher('apps_menu_imageviewer', 'apps_menu_utilities');
     # Check that is started
     assert_screen 'apps_run_imageviewer';
     # Register application
