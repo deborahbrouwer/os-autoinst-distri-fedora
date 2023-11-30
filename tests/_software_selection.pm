683234d7a5b11c46a01a0556b5478ca8e6becccf
Adam Williamson <awilliam@redhat.com>
2023-11-29 16:20:13 -0800

    Simplify package set selection in anaconda
    tab will both reach the environment selection list and then go
    through it, so we don't need to start out with tab and then
    switch to down. And since all we need to do is hit one key until
    a needle matches, let's use the handy function os-autoinst
    provides for doing this...
    
    This should fix it on current Rawhide (where it only takes one
    tab, or zero tabs - not sure which - to reach the list).
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -28,18 +28,10 @@ sub run {
     }
 
     assert_and_click "anaconda_main_hub_select_packages";
-
-    # Focus on "base environment" list
-    send_key "tab";
-    wait_still_screen 1;
-    send_key "tab";
-    wait_still_screen 1;
+    wait_still_screen 3;
 
     # select desired environment
-    # go through the list 20 times at max (to prevent infinite loop when it's missing)
-    for (my $i = 0; !check_screen("anaconda_${packageset}_highlighted", 1) && $i < 20; $i++) {
-        send_key "down";
-    }
+    send_key_until_needlematch "anaconda_${packageset}_highlighted", "tab";
 
     send_key "spc";
 
