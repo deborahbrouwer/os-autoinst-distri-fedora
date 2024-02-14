d998a56383c2971866aa657ca6176e88ab5d09ea
Adam Williamson <awilliam@redhat.com>
2024-02-14 13:35:32 -0800

    Add 41 to the workarounds table
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -477,6 +477,7 @@ sub get_workarounds {
         "38" => [],
         "39" => [],
         "40" => ["FEDORA-2024-912d46e3da", "FEDORA-2024-97337ce169", "FEDORA-2024-ca9110e3f3"],
+        "41" => [],
     );
     my $advortasks = $workarounds{$version};
     return @$advortasks;
