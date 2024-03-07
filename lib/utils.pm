02015ed5a058f0ec78e14e7c72014c8c6a396a3d
Adam Williamson <awilliam@redhat.com>
2024-03-07 15:54:51 -0800

    Add F40 backgrounds update as workaround
    So the kde-settings update can pass tests.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -476,7 +476,7 @@ sub get_workarounds {
     my %workarounds = (
         "38" => [],
         "39" => [],
-        "40" => [],
+        "40" => ["FEDORA-2024-f0fdde3a5d"],
         "41" => [],
     );
     my $advortasks = $workarounds{$version};
